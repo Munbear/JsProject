19543e40c39473e6215cd49cd4b912c69943b3da
Munbear <pdwssf@gmail.com>
2021-10-03 17:06:17 +0900

    modify key

--- clone_todolist/weather.js
+++ clone_todolist/weather.js
@@ -1,60 +1,57 @@
-const weather = document.querySelector(".js-weathe"); 
+const weather = document.querySelector('.js-weathe');
 
-const API_KEY = "89e63e98440510574577e91fd511245b";
+// const API_KEY = "api key here ";
 const COORDS = 'coords';
 
-function getWeather(lat, lng){
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
-    )
-    .then(function(response) {
-        return response.json();
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+  )
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(json){
-        const temperature = json.main.temp;
-        const place = json.name;
-        weather.innerText = `${temperature} @ ${place}`;
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
     });
 }
 
-function saveCoords(coordsObj){
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-
-function handleGeoSucces(position){
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const coordsObj = {
-        latitude,
-        longitude
-    };
-    saveCoords(coordsObj);
-    getWeather(latitude, longitude);
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
-function handleGeoError(){
-    console.log("Cant access geo location");
+function handleGeoError() {
+  console.log('Cant access geo location');
 }
 
-function askForCoords(){
-    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
 }
-
 
 function loadCoords() {
-    const loadedCoords = localStorage.getItem(COORDS);
-    if(loadedCoords === null){
-        askForCoords();
-    } else {
-        const parsedCoords = JSON.parse(loadedCoords);
-        getWeather(parsedCoords.latitude, parsedCoords.longitude);
-    }
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
 }
 
-
-function init(){   
-    loadCoords();
+function init() {
+  loadCoords();
 }
 
 init();
