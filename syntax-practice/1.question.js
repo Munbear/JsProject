49371eecd868a0c249609188c9b8f0dad604c996
Munbear <pdwssf@gmail.com>
2022-04-14 17:52:54 +0900

    for coding test

--- syntax-practice/1.question.js
+++ syntax-practice/1.question.js
@@ -1,6 +1,15 @@
-console.log('now Start!');
+/// s값에 숫자와 영어로 표기된 문자열이 섞여 있으면 영어로 표기된 글자를 숫자로 바꿔라
+function solution(s) {
+    let number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let answer = s;
+    for(let i=0; i < number.length; i++) {
+        let arr = answer.split(number[i]);
+        console.log(arr);
+        answer = arr.join(i);
+    }
 
-let a = "Hello";
-let b = "World";
-let result = a + b;
-console.log(result);
+    return Number(answer);
+}
+
+console.log(solution('12five8'));
+
