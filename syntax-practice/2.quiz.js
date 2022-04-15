2ffdadc591b805d33337d0d85294031fe9cc4fb9
Munbear <pdwssf@gmail.com>
2022-04-15 20:05:33 +0900

    for coding test

--- syntax-practice/2.quiz.js
+++ syntax-practice/2.quiz.js
@@ -1,7 +1,25 @@
-function solution(str) {
-    let count = 0;
-    
-    return count
+/**
+ * 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+ * 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+ *
+ * 제한조건
+ * x는 -10000000(천만) 이상, 10000000(천만) 이하인 정수이다.
+ * n는 1000 이하인 자연수이다.
+ */
+
+
+function solution(x,n) {
+    let result = [];
+    for(let i=0; i  < n; i++){
+        result.push(x*(i+1))
+    };
+    return result;
+};
+console.log(solution(-4,2))
+
+
+function bestAnswer(x, n) {
+    return Array(n).fill(x).map( (v, i) => (i + 1) * v);
 }
 
-console.log(solution('abcd'));
+console.log(bestAnswer(2,5));
