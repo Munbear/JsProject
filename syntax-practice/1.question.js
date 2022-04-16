6d78631ace222b2c95b8f9ecb87c8e142a4b012b
Munbear <pdwssf@gmail.com>
2022-04-16 18:39:28 +0900

    doing quis 6

--- syntax-practice/1.question.js
+++ syntax-practice/1.question.js
@@ -3,7 +3,7 @@ function solution(s) {
     let number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let answer = s;
     for(let i=0; i < number.length; i++) {
-        let arr = answer.split(number[i]);
+        let arr = answer.split(number[i]);j
         console.log(arr);
         answer = arr.join(i);
     }
---
+++ syntax-practice/6.quiz.js
@@ -0,0 +1,10 @@
+function numberOfCipher(num) {
+    let result = 0;
+    let splitNum = num.toString().split('',num.length);
+    for(let i=0; i < splitNum; i++){
+    }
+}
+
+
+numberOfCipher(123);
+// console.log(numberOfCipher(123));
