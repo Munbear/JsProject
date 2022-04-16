ebcbc60097e22d9cb83c788eca8af8001efaa7b8
Munbear <pdwssf@gmail.com>
2022-04-16 17:16:24 +0900

    practice coding test

--- syntax-practice/4.quiz.js
+++ syntax-practice/4.quiz.js
@@ -3,7 +3,16 @@
  */
 
 function average(arr) {
-    return Math.max(...arr) % 2 ;
+    let result = 0;
+    for(let i=0; i < arr.length; i++) {
+        result += arr[i]
+    }
+    return result/arr.length;
+}
+console.log('결과값 : ',average([5,5]));
+
+function bestAnswer(array) {
+    return array.reduce( (a,b) => a + b) /array.length;
 }
 
-console.log('결과값 : ',average([5,5]));
+console.log('best answer : ',bestAnswer([1,2,3,4]))
