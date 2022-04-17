221fe361528ecfbb08e4d0bcceb5b31f1e1b67d2
Munbear <pdwssf@gmail.com>
2022-04-17 14:17:46 +0900

    practice coding test

--- syntax-practice/6.quiz.js
+++ syntax-practice/6.quiz.js
@@ -1,10 +1,31 @@
-function numberOfCipher(num) {
-    let result = 0;
-    let splitNum = num.toString().split('',num.length);
-    for(let i=0; i < splitNum; i++){
+/**
+ * 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+ * n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+ */
+
+
+
+function square(n) {
+    if( n === Math.pow(Math.floor(Math.sqrt(n)), 2)){
+        return Math.pow(Math.floor(Math.sqrt(n))+ 1,2);
     }
+    return -1
 }
 
+console.log(square(121));
 
-numberOfCipher(123);
-// console.log(numberOfCipher(123));
+function otherAnswer(n) {
+    let result = 0;
+    let x =0
+    while(x*x < n){
+        x++;
+    }if(x*x == n){
+        x++;
+        result = x*x;
+    }else{
+        result = -1;
+    }
+    return result;
+}
+
+console.log('다른사람 풀이 : ',otherAnswer(3));
