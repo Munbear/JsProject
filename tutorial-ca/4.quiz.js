2a6dcb6ec353e420963b36bfe2143bcc9d8bba0c
Munbear <pdwssf@gmail.com>
2022-05-04 18:50:12 +0900

    learning js

--- tutorial-ca/4.quiz.js
+++ tutorial-ca/4.quiz.js
@@ -1,3 +1,5 @@
+// 문제 1.
+console.log('------------------------여기서부터 문제 1-------------------------');
 class Puppy {
     constructor(type,color) {
         this.type = type;
@@ -7,7 +9,9 @@ class Puppy {
         // instanceof 는 a instanceof b 이렇게 사용하면 a 가 b로부터 생성된 오브젝트인지 아닌지를
         // true/false로 알려주는 연산자 이다.
         if( this instanceof Kitty) {
-            this.age++
+            console.log(this.age++)
+        } else {
+            console.log('error');
         }
     }
 }
@@ -18,7 +22,7 @@ class Kitty extends Puppy {
         this.age = old;
     }
     getAge() {
-        super.getAge();
+        this.age ++
     }
 }
 
@@ -26,14 +30,17 @@ let martise = new Puppy('말티즈','white');
 let jindo = new Puppy('진돗개','brown');
 let coshort = new Kitty('코숏','white',5);
 let russianblue = new Kitty('러시안블루','gary',1);
-
-
 console.log(martise)
-console.log(jindo);
-console.log(coshort);
-console.log(russianblue);
+console.log(jindo)
+console.log(coshort)
+console.log(russianblue)
+martise.getAge();
+jindo.getAge();
 
 
+
+
+// 문제 2
 class Unit {
     constructor() {
         this.power = 5;
@@ -43,19 +50,26 @@ class Unit {
         return this.power + this.strenght;
     }
     set heal(hp) {
-        this.strenght = this.strenght + hp;
+        return this.strenght = this.strenght + hp;
     }
 }
 
+console.log('----------밑에서부터 문제 2----------------');
 let unitA = new Unit();
 let unitB = new Unit();
 
 console.log(unitA);
 console.log(unitB);
-console.log(unitA.battlePoint);
-console.log(unitB.battlePoint);
+console.log('unitA의 총 능력치는 : ',unitA.battlePoint);
+console.log('unitB의 총 능력치는 : ',unitB.battlePoint);
 unitA.heal = 50;
-////////////////////////
+unitB.heal = 30;
+console.log(unitA)
+console.log(unitB)
+console.log('unit의 총 능력치는 : ',unitA.battlePoint);
+console.log('unit의 총 능력치는 : ',unitB.battlePoint)
+// 문제3.
+console.log('-------------------------밑에서부터 문제3 ------------------------------');
 let data = {
     odd: [],
     even: [],
