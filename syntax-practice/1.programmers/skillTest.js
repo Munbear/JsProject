b34d21caa43acae3a4f0647942b949d66501aacb
Munbear <pdwssf@gmail.com>
2022-07-15 17:22:03 +0900

    practice algorithm

--- syntax-practice/1.programmers/skillTest.js
+++ syntax-practice/1.programmers/skillTest.js
@@ -2,18 +2,21 @@
 // 예 [1,1,3,3,0,1,1] => [1,3,0,1]
 // 예2 [4,4,4,3,3] => [4,3]
 
+/**
+ * 배열에서 다음 인덱스 값이 같으면 빈 배열에 값을 추가 한다.
+ * 문제1: 중복된 모든 값이 지워짐 => [1,3,0]
+ *  원하는 값은 [1,3,0,1] 뒤에 있는 중복 값도 나와야 함
+ * **/
+
 function removeElement(arr) {
-    let result = []
-    arr.forEach( (value) => {
-        if( !result.includes(value)) {
-            return result.push(value)
-        }
+    // return arr.filter( (e,i) => arr[i+1] !== e)
+    arr.filter( (e,i) => {
+        console.log(arr[i+1])
     })
-    return result
 }
 
 
 console.log(removeElement([1,1,3,3,0,1,1]));
 // removeElement([1,1,3,3,0,1,1]);
 
-// removeElement([4,4,4,3,3])
+// console.log(removeElement([4,4,4,3,3]))
