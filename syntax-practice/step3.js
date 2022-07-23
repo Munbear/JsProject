a2b0d1a7451b7748cc62af55bab543885a662dfc
Munbear <pdwssf@gmail.com>
2022-07-23 19:11:50 +0900

    practice algorithm

---
+++ syntax-practice/step3.js
@@ -0,0 +1,12 @@
+/**
+ * 배열에서 가장 작은 수를 제거하고 나머지 배열을 리턴하라
+ * 만약 리턴하는 배열이 빈 배열이거나 [10] 이면 -1 를 리턴하세요
+ * **/
+
+function solution(arr) {
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
+    return arr.length ? arr : [-1];
+};
+
+console.log(solution([4,3,2,1,]))
+console.log(solution([10]))
