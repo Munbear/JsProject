310aad8e38dc3517de4c135c110b2d2b2745ba6d
Munbear <pdwssf@gmail.com>
2022-07-25 19:10:40 +0900

    practice algorithm

--- syntax-practice/step1.js
+++ syntax-practice/step1.js
@@ -1,21 +1,23 @@
-/**
- * level one 소수 만들기
- * **/
+// [ 소수 찾기 ]
+// 1 부터 n 사이의 소수의 개수를 반환 하는 함수를 작성하시오
 
-function solution(nums) {
-    for(let i=0; i < nums.length; i++){
-        if(nums[i] == 1){
-            return false
-        } else if (nums[i] == 2){
-            return true
-        } else {
-
+function findPrimeNum(n) {
+    // set은 데이터의 집합체 중복X
+    // set의 prototype = has/add/delete ...
+    const primeNum = new Set()
+    for(let i=3 ; i <= n; i +=2 ) {
+        primeNum.add(i);
+    }
+    primeNum.add(2);
+    for (let j=3; j ** 2 < n; j++){
+        if(primeNum.has(j)){
+            for(let k =j ** 2; k <= n; k+=j){
+                primeNum.delete(k)
+            }
         }
     }
-
-    // return result
+    return primeNum.size;
 }
 
-// logsolution([1,2,3,4]);
-console.log(solution([1,2,3,4]))
-//solution([1,2,7,6,4])
+console.log(findPrimeNum(10))
+console.log(findPrimeNum(5));
